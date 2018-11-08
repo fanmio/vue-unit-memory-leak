26072b952e29a1b3f10c97e0c3779633b038774c
Christian Gambardella <christian.gambardella@gmail.com>
2018-11-08 17:36:35 +0100

    adds test setup

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,12 +2,32 @@ import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
+// xdescribe('HelloWorld.vue', () => {
+//   for (let i = 0; i < 10000; i++) {
+//     it(`renders props.msg when passed: ${i}`, () => {
+//       const msg = 'new message'
+//       const wrapper = shallowMount(HelloWorld, {
+//         propsData: { msg }
+//       })
+//       console.log('heapUsed', process.memoryUsage().heapUsed)
+//       expect(wrapper.text()).to.include(msg)
+//     })
+//   }
+// })
+
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+  const msg = 'new message'
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
-    expect(wrapper.text()).to.include(msg)
   })
+
+  for (let i = 0; i < 10000; i++) {
+    it(`renders props.msg when passed: ${i}`, () => {
+      console.log('heapUsed', process.memoryUsage().heapUsed)
+      expect(wrapper.text()).to.include(msg)
+    })
+  }
 })
